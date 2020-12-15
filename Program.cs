c301e6a55a176613be6411fa4fa219e4528e7e8f
DEV <dev@info.local>
2020-12-15 09:38:46 -0300

    Exercício de revisão divisão concluído

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lacos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("--- Bem vindo ao nosso sistema de calculadora virtual ---");
+            Console.Write("Que conta de divisão deseja ver? ");
+            float resposta = float.Parse(Console.ReadLine());
+
+            float divisao;
+
+            for (int contador = 0; contador <=20; contador++){
+            divisao = resposta / contador;
+            Console.WriteLine($"{resposta} / {contador} = {divisao}");
+
+            }
+
+        }
+    }
+}
